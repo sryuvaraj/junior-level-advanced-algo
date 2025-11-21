1e6862c67b0bf5174ca649446d6d83ef50d29583
sryuvaraj <sryuvaraj99@gmail.com>
2025-11-21 20:47:05 +0530

    tow sum

---
+++ TwoSum.js
@@ -0,0 +1,17 @@
+// Two sum → [2,7,11,15],9 → [0,1]
+
+var input = [2, 7, 11, 15];
+var target = 18;
+var arr = [];
+var count = 0;
+for (let i = 0; i < input.length; i++) {
+  for (let j = ; i < input.length; i++) {
+    var sum = input[i] + input[j];
+    if (sum === target) {
+      arr[0] = input[i];
+      arr[1] = input[j];
+    }
+  }
+}
+
+console.log(arr);
