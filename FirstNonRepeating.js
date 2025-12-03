76c8cda4d8634403aaa747bd46cedfffb1d75ab3
sryuvaraj <sryuvaraj99@gmail.com>
2025-12-03 12:07:30 +0530

    corrected first non repeating

--- FirstNonRepeating.js
+++ FirstNonRepeating.js
@@ -1,18 +1,33 @@
-const input = [6, 6, 2, 2, 3];
+const input = [6, 6, 2, 2, 3, 1];
 
-let first = -1;
-
-for (let i = 0; i < input.length; i++) {
-  let count = 0;
-  for (let j = 0; j < input.length; j++) {
-    if (input[i] === input[j]) {
-      count++;
-    }
-  }
-  if (count === 1) {
-    first = input[i];
-    break;
+var big = input[0]
+for(el of input){
+  if(el>big){
+    big=el
   }
 }
 
-console.log(first);  // 3
+var small = input[0]
+for(el of input){
+  if(el<small){
+    small=el
+  }
+}
+
+var result = input[0]
+
+for(let i = small; i<=big; i++){
+  var p = input[i-1]
+  var c = input[i]
+  var n = input[i+1]
+  if((p===c)||(c===n)){
+    // console.log(c)
+  }
+  else{
+    result=c
+    break
+  }
+}
+
+
+console.log(result)
